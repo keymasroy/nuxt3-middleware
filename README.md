caaa199f481ff430ea2b064ca43004e4c044e6e1
keymasroy <keymasroy@gmail.com>
2022-09-28 17:03:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt3-middleware
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-3-drjzew)
---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+<h2>Hallo Welt<h2/>
+<nuxt-link to="/">Back<nuxt-link/>
+</div>
+
+</template>
